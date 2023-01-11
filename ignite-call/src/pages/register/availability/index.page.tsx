b802b4753c0e71279adaf16d18fd275c9ef16203
Bruno Mariani <bruno0907@gmail.com>
2023-01-10 22:57:46 -0300

    chore: added default values for availability array

--- ignite-call/src/pages/register/availability/index.page.tsx
+++ ignite-call/src/pages/register/availability/index.page.tsx
@@ -18,8 +18,51 @@ import {
   AvailabilityItem,
 } from './styles'
 import { Container, Header } from '../styles'
+import { useForm, useFieldArray } from 'react-hook-form'
+import * as z from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { getWeekDays } from '../../../utils/get-week-days'
+
+const availabilityFormSchema = z.object({})
+
+type AvailabilityFormProps = z.infer<typeof availabilityFormSchema>
 
 export default function Availability() {
+  const {
+    register,
+    handleSubmit,
+    control,
+    formState: { isSubmitting },
+  } = useForm({
+    resolver: zodResolver(availabilityFormSchema),
+    defaultValues: {
+      availability: [
+        { weekDay: 0, enabled: false, startTime: '08:00', endTime: '17:00' },
+        { weekDay: 1, enabled: true, startTime: '07:00', endTime: '16:00' },
+        { weekDay: 2, enabled: false, startTime: '09:00', endTime: '18:00' },
+        { weekDay: 3, enabled: true, startTime: '07:00', endTime: '16:00' },
+        { weekDay: 4, enabled: false, startTime: '10:00', endTime: '19:00' },
+        { weekDay: 5, enabled: true, startTime: '09:00', endTime: '15:00' },
+        { weekDay: 6, enabled: false, startTime: '00:00', endTime: '00:00' },
+      ],
+    },
+  })
+
+  const { fields } = useFieldArray({
+    control,
+    name: 'availability',
+  })
+
+  const weekDay = getWeekDays()
+
+  function handleSetAvailability(data: AvailabilityFormProps) {
+    console.log({ data })
+  }
+
+  function handleSetAvailabilityErrors(error: AvailabilityFormProps) {
+    console.log({ error })
+  }
+
   return (
     <>
       <Head>
@@ -34,33 +77,49 @@ export default function Availability() {
           </Text>
           <MultiStep currentStep={3} size={4} />
         </Header>
-        <AvailabilityBox as="form">
+        <AvailabilityBox
+          as="form"
+          onSubmit={handleSubmit(
+            handleSetAvailability,
+            handleSetAvailabilityErrors,
+          )}
+        >
           <AvailabilityContainer>
-            {['Segunda-feira', 'Terça-feira', 'Quarta-feira'].map((day) => {
-              return <AvailabilityComponent key={day} day={day} />
+            {fields.map((availability) => {
+              return (
+                <AvailabilityItem key={availability.id}>
+                  <AvailabilityDay>
+                    <Checkbox checked={availability.enabled} />
+                    <Text size="sm">{weekDay[availability.weekDay]}</Text>
+                  </AvailabilityDay>
+                  <AvailabilityInputs>
+                    <TextInput
+                      size="sm"
+                      type="time"
+                      step={60}
+                      {...register(
+                        `availability.${availability.weekDay}.startTime`,
+                      )}
+                    />
+                    <TextInput
+                      size="sm"
+                      type="time"
+                      step={60}
+                      {...register(
+                        `availability.${availability.weekDay}.endTime`,
+                      )}
+                    />
+                  </AvailabilityInputs>
+                </AvailabilityItem>
+              )
             })}
           </AvailabilityContainer>
-          <Button>
+          <Button disabled={isSubmitting}>
             Próximo Passo
             <ArrowRight />
           </Button>
         </AvailabilityBox>
       </Container>
     </>
-  )
-}
-
-const AvailabilityComponent = ({ day }: { day: string }) => {
-  return (
-    <AvailabilityItem>
-      <AvailabilityDay>
-        <Checkbox />
-        <Text size="sm">{day}</Text>
-      </AvailabilityDay>
-      <AvailabilityInputs>
-        <TextInput size="sm" type="time" step={60} />
-        <TextInput size="sm" type="time" step={60} />
-      </AvailabilityInputs>
-    </AvailabilityItem>
   )
 }
---
+++ ignite-call/src/utils/get-week-days.ts
@@ -0,0 +1,13 @@
+export function getWeekDays() {
+  const formatter = new Intl.DateTimeFormat('pt-br', { weekday: 'long' })
+
+  const date = new Date()
+  const year = date.getFullYear()
+  const month = date.getMonth() + 1 // In JS month starts at 0 so January = 0, February = 1 and so on.
+
+  return Array.from(Array(7).keys())
+    .map((day) => formatter.format(new Date(Date.UTC(year, month, day))))
+    .map((weekDay) =>
+      weekDay.substring(0, 1).toUpperCase().concat(weekDay.substring(1)),
+    )
+}
