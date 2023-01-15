d20a0529d02409e19f952ddb3bfb6fafca6a4c34
Bruno Mariani <bruno0907@gmail.com>
2023-01-15 17:22:03 -0300

    feat: added dynamic week and days from dayjs to calendar

--- ignite-call/src/components/Calendar/index.tsx
+++ ignite-call/src/components/Calendar/index.tsx
@@ -1,7 +1,7 @@
 import { Text } from '@ignite-ui/react'
-import dayjs from 'dayjs'
+import dayjs, { Dayjs } from 'dayjs'
 import { CaretLeft, CaretRight } from 'phosphor-react'
-import { useState } from 'react'
+import { useMemo, useState } from 'react'
 import { getWeekDays } from '../../utils/get-week-days'
 import {
   CalendarActions,
@@ -12,11 +12,71 @@ import {
   CalendarTitle,
 } from './styles'
 
+interface CalendarDayProps {
+  day: Dayjs
+  disabled: boolean
+}
+interface CalendarWeeksProps {
+  week: number
+  days: CalendarDayProps[]
+}
+
 export function Calendar() {
   const [currentDate, setCurrentDate] = useState(() => dayjs().set('date', 1))
 
   const currentMonth = currentDate.format('MMMM')
   const currentYear = currentDate.format('YYYY')
+
+  const calendarWeeks = useMemo(() => {
+    const daysInMonthArray = Array.from({
+      length: currentDate.daysInMonth(),
+    })
+      .map((_, i) => currentDate.set('date', i + 1))
+      .map((day) => ({ day, disabled: false }))
+
+    const firstWeekDay = currentDate.get('day')
+
+    const previousMonthFillArray = Array.from({ length: firstWeekDay })
+      .map((_, i) => currentDate.subtract(i + 1, 'day'))
+      .reverse()
+      .map((day) => ({ day, disabled: true }))
+
+    const lastDayInCurrentMonth = currentDate.set(
+      'date',
+      currentDate.daysInMonth(),
+    )
+    const lastWeekDay = lastDayInCurrentMonth.get('day')
+
+    const nextMonthFillArray = Array.from({
+      length: 7 - (lastWeekDay + 1),
+    })
+      .map((_, i) => lastDayInCurrentMonth.add(i + 1, 'day'))
+      .map((day) => ({ day, disabled: true }))
+
+    const calendarDays = [
+      ...previousMonthFillArray,
+      ...daysInMonthArray,
+      ...nextMonthFillArray,
+    ]
+
+    const calendarWeeks = calendarDays.reduce<CalendarWeeksProps[]>(
+      (weeks, _, i, original) => {
+        const isNewWeek = i % 7 === 0
+
+        if (isNewWeek) {
+          const newWeek = {
+            week: i / 7 + 1,
+            days: original.slice(i, i + 7),
+          }
+          weeks.push(newWeek)
+        }
+        return weeks
+      },
+      [],
+    )
+
+    return calendarWeeks
+  }, [currentDate])
 
   function handlePreviousMonth() {
     const previousMonth = currentDate.subtract(1, 'month')
@@ -56,13 +116,15 @@ export function Calendar() {
           </tr>
         </thead>
         <tbody>
-          {Array.from({ length: 4 }).map((_, i) => {
+          {calendarWeeks.map(({ week, days }) => {
             return (
-              <tr key={i}>
-                {Array.from({ length: 7 }).map((_, i) => {
+              <tr key={week}>
+                {days.map(({ day, disabled }) => {
                   return (
-                    <td key={i}>
-                      <CalendarDay>{i}</CalendarDay>
+                    <td key={day.date()}>
+                      <CalendarDay disabled={disabled}>
+                        {day.date()}
+                      </CalendarDay>
                     </td>
                   )
                 })}
