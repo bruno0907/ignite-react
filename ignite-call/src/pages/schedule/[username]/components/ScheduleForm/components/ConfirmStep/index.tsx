f2bde13481072ffd44679f7adef61f78c3e4f734
Bruno Mariani <bruno0907@gmail.com>
2023-01-14 23:06:59 -0300

    feat: added confirm step component

---
+++ ignite-call/src/pages/schedule/[username]/components/ScheduleForm/components/ConfirmStep/index.tsx
@@ -0,0 +1,76 @@
+import { zodResolver } from '@hookform/resolvers/zod'
+import { Button, Text, TextArea, TextInput } from '@ignite-ui/react'
+import { CalendarBlank, Clock } from 'phosphor-react'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+import { ConfirmForm, FormActions, FormHeader, FormError } from './styles'
+
+const confirmStepSchema = z.object({
+  name: z
+    .string()
+    .min(3, { message: 'O nome precisa ter no mínimo 3 caracteres.' }),
+  email: z.string().email({ message: 'Digite um e-mail válido.' }),
+  observations: z.string().nullable(),
+})
+
+type ConfirmStepFormProps = z.infer<typeof confirmStepSchema>
+
+export function ConfirmStep() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm<ConfirmStepFormProps>({
+    resolver: zodResolver(confirmStepSchema),
+  })
+  function handleConfirmScheduling(data: ConfirmStepFormProps) {
+    console.log(data)
+  }
+
+  return (
+    <ConfirmForm as="form" onSubmit={handleSubmit(handleConfirmScheduling)}>
+      <FormHeader>
+        <Text>
+          <CalendarBlank />
+          14 de Janeiro de 2023
+        </Text>
+        <Text>
+          <Clock />
+          18:00h
+        </Text>
+      </FormHeader>
+
+      <label>
+        <Text size="sm">Nome completo</Text>
+        <TextInput placeholder="Seu nome" {...register('name')} />
+        {errors.name && <FormError size="sm">{errors.name.message}</FormError>}
+      </label>
+
+      <label>
+        <Text size="sm">Endereço de e-mail</Text>
+        <TextInput
+          type="email"
+          placeholder="johndoe@example.com"
+          {...register('email')}
+        />
+        {errors.email && (
+          <FormError size="sm">{errors.email.message}</FormError>
+        )}
+      </label>
+
+      <label>
+        <Text size="sm">Observações</Text>
+        <TextArea {...register('observations')} />
+      </label>
+
+      <FormActions>
+        <Button type="button" variant="tertiary">
+          Cancelar
+        </Button>
+        <Button type="submit" disabled={isSubmitting}>
+          Confirmar
+        </Button>
+      </FormActions>
+    </ConfirmForm>
+  )
+}
--- ignite-call/src/pages/schedule/[username]/components/ScheduleForm/index.tsx
+++ ignite-call/src/pages/schedule/[username]/components/ScheduleForm/index.tsx
@@ -1,5 +1,7 @@
 import { CalendarStep } from './components/CalendarStep'
+import { ConfirmStep } from './components/ConfirmStep'
 
 export function ScheduleForm() {
-  return <CalendarStep />
+  // return <CalendarStep />
+  return <ConfirmStep />
 }
